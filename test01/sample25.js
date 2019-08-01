17e5deed0693e9cb2244c4597fea422eb43b0293
Your Name <you@example.com>
2019-08-01 10:31:54 +0900

    START REACT

--- test01/sample25.js
+++ test01/sample25.js
@@ -1,8 +1,32 @@
-function ff(n, k, s) {
-  this.n = n; //지금 만들어진 n , 매개변수 n
+function ff(n, k, e) {  // 생성자함수
+  this.n = n;
   this.k = k;
-this.s = function (param) {  }
-
-this.output = function()
- }
+  this.e = e;
+  this.s = function () {
+      return this.k + this.e;
+  },
+  this.output = function () {
+      console.log(this.n, this.k, this.e, this.s())
+  }
+}
+// 사용법
+// const obj8 = new ff('고양이', 30, 20);
+// console.log(obj8);  // 함수객체
  //함수를 new 해서 call하는 것은 생성자 함수이다
+
+
+
+ /**es6 에서는 클래스로 표현함*/
+//  class ff {
+//   constructor(n, k, e) {
+//     this.n = n;
+//     this.k = k;
+//     this.e = e;
+//     this.s = function () {
+//       return this.k + this.e;
+//     },
+//       this.output = function () {
+//         console.log(this.n, this.k, this.e, this.s());
+//       };
+//   }
+// }
